1ee182ab389c5abca7da3a86e7abf2230f62d6af
stone <414899490@qq.com>
2019-03-27 16:22:11 +0800

    spring-boot-04-web-restfulcrud

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -1,9 +1,11 @@
 package com.atguigu.springboot.config;
 
+import com.atguigu.springboot.component.LoginHandleInterceptor;
 import com.atguigu.springboot.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -32,8 +34,19 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("login");
                 registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            // 注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //super.addInterceptors(registry);
+                registry.addInterceptor(new LoginHandleInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html", "/", "/user/login");
+
             }
         };
+
         return adapter;
     }
 
