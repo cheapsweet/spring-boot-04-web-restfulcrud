b71b24b9b9a20e01a085578ebf1af2a5987e0d11
stone <414899490@qq.com>
2019-03-27 19:30:57 +0800

    spring-boot-04-web-restfulcrud

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.atguigu.springboot.controller;
 
+import com.atguigu.springboot.dao.DepartmentDao;
 import com.atguigu.springboot.dao.EmployeeDao;
+import com.atguigu.springboot.entities.Department;
 import com.atguigu.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,12 +17,24 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps", employees);
 
         return "emp/list";
+    }
+
+    // 来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        // 来到添加页面，查出所有的部门
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
     }
 
 }
