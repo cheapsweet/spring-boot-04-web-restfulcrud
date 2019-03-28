46cb1ae0b9e57a9d2288579144b769880e5088bb
stone <414899490@qq.com>
2019-03-28 20:42:48 +0800

    41、尚硅谷_SpringBoot_web开发-【实验】-员工添加-添加完成

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -33,8 +34,26 @@ public class EmployeeController {
     public String toAddPage(Model model) {
         // 来到添加页面，查出所有的部门
         Collection<Department> departments = departmentDao.getDepartments();
+        //System.out.println(" [打印departments] ");
+        //System.out.println(departments.toString());
         model.addAttribute("depts", departments);
         return "emp/add";
+    }
+
+    // 员工添加
+    // springMVC自动将请求参数和入参对象的属性进行一一绑定，
+    // 要求请求参数的名字和javabean的属性一致
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+
+        // 来到员工列表页面
+
+        System.out.println("保存的员工信息：" + employee);
+        employeeDao.save(employee);
+
+        // redirect: 表示重定向到一个地址 /代表当前项目路径
+        // forward: 表示转发到一个地址
+        return "redirect:/emps";
     }
 
 }
