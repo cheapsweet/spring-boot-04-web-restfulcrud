4cc325ea03203c1a3c9886564e29addbe2512128
stone <414899490@qq.com>
2019-03-29 11:53:36 +0800

    42、尚硅谷_SpringBoot_web开发-【实验】-员工修改-重用页面&修改完成

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -21,6 +23,7 @@ public class EmployeeController {
     @Autowired
     DepartmentDao departmentDao;
 
+    // 员工列表页面
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
@@ -29,13 +32,11 @@ public class EmployeeController {
         return "emp/list";
     }
 
-    // 来到员工添加页面
+    // 员工添加页面
     @GetMapping("/emp")
     public String toAddPage(Model model) {
         // 来到添加页面，查出所有的部门
         Collection<Department> departments = departmentDao.getDepartments();
-        //System.out.println(" [打印departments] ");
-        //System.out.println(departments.toString());
         model.addAttribute("depts", departments);
         return "emp/add";
     }
@@ -45,14 +46,34 @@ public class EmployeeController {
     // 要求请求参数的名字和javabean的属性一致
     @PostMapping("/emp")
     public String addEmp(Employee employee) {
-
         // 来到员工列表页面
-
         System.out.println("保存的员工信息：" + employee);
         employeeDao.save(employee);
-
         // redirect: 表示重定向到一个地址 /代表当前项目路径
         // forward: 表示转发到一个地址
+        return "redirect:/emps";
+    }
+
+    // 来到修改页面，查出当前员工，在页面回显
+    // 以路径变量的形式传id这个参数
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+
+        // 来到添加页面，查出所有的部门
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+
+        // 回到修改页面(add是一个修改和添加的页面)
+        return "emp/add";
+    }
+
+    // 员工修改:需要提交员工id
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee) {
+        System.out.println("修改的员工数据：" + employee);
+        employeeDao.save(employee);
         return "redirect:/emps";
     }
 
